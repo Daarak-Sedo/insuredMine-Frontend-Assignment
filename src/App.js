9611dad84e03ac87d751639bd3a79c693970d3a7
Bhuwan Singh Samant <bhuwansinghsamant@Bhuwans-MacBook-Air.local>
2023-04-21 11:53:02 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const agentNames = [
+  "GinaWilliams",
+  "Jake Williams",
+  "Jamie John",
+  "John Doe",
+  "Jeff Stewart",
+  "Paula M. Keith"
+];
+
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    console.log(value)
+    setInputValue(value);
+    setShowDropdown(value.trim().startsWith("@"));
+    setSelectedIndex(-1);
+  };
+
+  const handleKeyDown = (event) => {
+    const { keyCode } = event;
+    if (keyCode === 38 && selectedIndex > 0) {
+      setSelectedIndex(selectedIndex - 1);
+    } else if (keyCode === 40 && selectedIndex < agentNames.length - 1) {
+      setSelectedIndex(selectedIndex + 1);
+    } else if (keyCode === 13 && selectedIndex >= 0) {
+      setInputValue(agentNames[selectedIndex]);
+      setShowDropdown(false);
+    }
+  };
+
+  const handleAgentClick = (agentName) => {
+    setInputValue(agentName);
+    setShowDropdown(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+  <span>
+        <textarea
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Type @ to find Agent..."
+        />
+</span>
+       
+        {showDropdown && (
+          <ul className="dropdown">
+            {agentNames.map((agentName, index) => (
+              <li
+                key={agentName}
+                className={selectedIndex === index ? "selected" : ""}
+                onClick={() => handleAgentClick(agentName)}
+              >
+              
+                {agentName}
+       
+               
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
